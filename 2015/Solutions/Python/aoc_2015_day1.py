3de9bbeea415df96f1d8c5ed861e07bee8c26bc4
sco1 <sco1.git@gmail.com>
2019-06-09 14:11:16 -0400

    Add 2015 Day 1

---
+++ 2015/Solutions/Python/aoc_2015_day1.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+
+def santa_elevator(instructions: str) -> int:
+    """
+    Help Santa navigate the large apartment building!
+
+    `instructions` is a string of parentheses, where ( goes up one floor and ) goes down one floor
+
+    Returns the destination floor
+    """
+    current_floor = 0
+    for step in instructions:
+        if step == "(":
+            current_floor += 1
+        elif step == ")":
+            current_floor -= 1
+        else:
+            raise ValueError(f"Unknown instruction: '{step}'")
+
+    return current_floor
+
+
+def basement_finder(instructions: str) -> int:
+    """
+    Find where Santa enters the basement!
+
+    `instructions` is a string of parentheses, where ( goes up one floor and ) goes down one floor
+
+    Returns the step index where Santa first enters the basement (floor = -1)
+    """
+    current_floor = 0
+    for idx, step in enumerate(instructions):
+        if step == "(":
+            current_floor += 1
+        elif step == ")":
+            current_floor -= 1
+        else:
+            raise ValueError(f"Unknown instruction: '{step}'")
+
+        if current_floor == -1:
+            return idx + 1
+
+
+puzzle_input_file = Path("../../Inputs/puzzle_input_d1.txt")
+with puzzle_input_file.open(mode="r") as f:
+    puzzle_input = f.read()
+
+print(santa_elevator(puzzle_input))
+print(basement_finder(puzzle_input))
