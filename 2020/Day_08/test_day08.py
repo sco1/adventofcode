904772b9481fa97ce855dc1070b6d94d23356690
S. Co1 <sco1.git@gmail.com>
2020-12-08 10:58:39 -0500

    Add 2020 Day 8

---
+++ 2020/Day_08/test_day08.py
@@ -0,0 +1,37 @@
+from textwrap import dedent
+
+from .aoc_2020_day08 import GameGear, RepeatInstructionError, mutate_until_fixed
+
+
+SAMPLE_INSTRUCTIONS = dedent(
+    """\
+    nop +0
+    acc +1
+    jmp +4
+    acc +3
+    jmp -3
+    acc -99
+    acc +1
+    jmp -4
+    acc +6
+    """
+)
+
+MACHINE = GameGear(SAMPLE_INSTRUCTIONS)
+
+
+def test_infinite_loop_termination() -> None:
+    """Test for correct accumulator value after machine reaches its first repeated instruction."""
+    MACHINE.reset()
+
+    try:
+        MACHINE.run()
+    except RepeatInstructionError:
+        pass
+
+    assert MACHINE.accumulator == 5
+
+
+def test_corruption_fixing() -> None:
+    """Test that the machine is correctly fixed & runs the instruction set to completion."""
+    assert mutate_until_fixed(SAMPLE_INSTRUCTIONS) == 8
