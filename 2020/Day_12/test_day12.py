5956a37a76d9f027e467e57a5075a3e6725109cf
S. Co1 <sco1.git@gmail.com>
2020-12-12 12:27:28 -0500

    Finish AOC 2020 Day 12

--- 2020/Day_12/test_day12.py
+++ 2020/Day_12/test_day12.py
@@ -1,6 +1,6 @@
 from textwrap import dedent
 
-import pytest
+from .aoc_2020_day12 import AdventFerry
 
 
 SAMPLE_INSTRUCTIONS = dedent(
@@ -12,3 +12,19 @@ SAMPLE_INSTRUCTIONS = dedent(
     F11
     """
 )
+
+
+def test_ferry_driving() -> None:
+    """Test Part 1, where the instructions drive the ship itself."""
+    advent_cruise = AdventFerry(SAMPLE_INSTRUCTIONS)
+    advent_cruise.run()
+
+    assert advent_cruise.distance_from_start() == 25
+
+
+def test_waypoint_driving() -> None:
+    """Test Part 2, where the instructions manipulate the ship's steering waypoint."""
+    advent_cruise = AdventFerry(SAMPLE_INSTRUCTIONS, waypoint_mode=True)
+    advent_cruise.run()
+
+    assert advent_cruise.distance_from_start() == 286
