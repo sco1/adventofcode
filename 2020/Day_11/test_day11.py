d4a467753f76b3f40f5852b3d488f39ce2135f32
S. Co1 <sco1.git@gmail.com>
2020-12-12 13:31:39 -0500

    Add 2020 Day 11 Part 1

--- 2020/Day_11/test_day11.py
+++ 2020/Day_11/test_day11.py
@@ -1,6 +1,6 @@
 from textwrap import dedent
 
-import pytest
+from .aoc_2020_day11 import SeatMap
 
 
 SEAT_LAYOUT = dedent(
@@ -17,3 +17,11 @@ SEAT_LAYOUT = dedent(
     L.LLLLL.LL
     """
 )
+
+
+def test_seat_stabilization() -> None:
+    """Check the number of occupied seats after the Part One boarding process has stabilized."""
+    seat_map = SeatMap(SEAT_LAYOUT)
+    seat_map.board_until_stable()
+
+    assert seat_map.n_occupied() == 37
