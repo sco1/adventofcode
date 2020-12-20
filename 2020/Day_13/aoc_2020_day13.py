faf19db4cf1803af39816fd3279351261aeedfa0
S. Co1 <sco1.git@gmail.com>
2020-12-20 16:34:42 -0500

    Add 2020 Day 13 Part 2

--- 2020/Day_13/aoc_2020_day13.py
+++ 2020/Day_13/aoc_2020_day13.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 
+from sympy.ntheory.modular import crt
+
 
 def parse_bus_schedule(raw_schedule: str) -> tuple[int, list[int]]:
     """
@@ -42,11 +44,35 @@ def find_bus_id(leave_time: int, bus_ids: list[int]) -> tuple[int, int]:
     return bus_id, min_wait_time
 
 
+def find_golden_timestamp(bus_schedule: str) -> int:
+    """
+    Find the earliest timestamp where bus departures flow according to their schedule position.
+
+    For example, given the schedule `"7,x,2,11"` we want to find the earliest timestamp, `t`, where
+    Bus 7 departs at `t`, Bus 2 departs at `t+2`, and Bus 11 departs at `t+3`.
+
+    NOTE: For simplification purposes, Bus IDs are assumed to be prime
+    """
+    parsed_schedule = [
+        (int(bus_id), -1 * offset)  # Negate offset so CRT will calculate appropriately
+        for offset, bus_id in enumerate(bus_schedule.split(","))
+        if bus_id != "x"
+    ]
+
+    # I used these links to help try and get an understanding of what is happening with the maths:
+    #    https://eli.thegreenplace.net/2019/the-chinese-remainder-theorem/
+    #    https://en.wikipedia.org/wiki/Modular_multiplicative_inverse
+    bus_ids = [slot[0] for slot in parsed_schedule]
+    offsets = [slot[1] for slot in parsed_schedule]
+    return crt(bus_ids, offsets, check=False)[0]
+
+
 if __name__ == "__main__":
     puzzle_input_file = Path("./puzzle_input.txt")
     puzzle_input = puzzle_input_file.read_text()
 
     leave_time, bus_ids = parse_bus_schedule(puzzle_input)
     bus_id, min_wait_time = find_bus_id(leave_time, bus_ids)
-
     print(f"Part One: {bus_id * min_wait_time}")
+
+    print(f"Part Two: t={find_golden_timestamp(puzzle_input.splitlines()[1])}")
--- 2020/Day_13/test_day13.py
+++ 2020/Day_13/test_day13.py
@@ -1,6 +1,8 @@
 from textwrap import dedent
 
-from .aoc_2020_day13 import find_bus_id, parse_bus_schedule
+import pytest
+
+from .aoc_2020_day13 import find_bus_id, find_golden_timestamp, parse_bus_schedule
 
 
 SAMPLE_SCHEDULE = dedent(
@@ -16,3 +18,18 @@ def test_part_one() -> None:  # noqa: D103
     bus_id, min_wait_time = find_bus_id(leave_time, bus_ids)
 
     assert (bus_id * min_wait_time) == 295
+
+
+PART_TWO_SAMPLES = [
+    ("7,13,x,x,59,x,31,19", 1068781),
+    ("17,x,13,19", 3417),
+    ("67,7,59,61", 754018),
+    ("67,x,7,59,61", 779210),
+    ("67,7,x,59,61", 1261476),
+    ("1789,37,47,1889", 1202161486),
+]
+
+
+@pytest.mark.parametrize(("bus_schedule", "check_timestamp"), PART_TWO_SAMPLES)
+def test_part_two(bus_schedule: str, check_timestamp: int) -> None:  # noqa: D103
+    assert find_golden_timestamp(bus_schedule) == check_timestamp
