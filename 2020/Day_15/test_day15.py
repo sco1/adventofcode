947c547b9af8b2d2034c71a252493ccff5422a52
S. Co1 <sco1.git@gmail.com>
2020-12-21 13:35:01 -0500

    Add 2020 Day 15
    It's a bit slow lol

--- 2020/Day_15/test_day15.py
+++ 2020/Day_15/test_day15.py
@@ -1,3 +1,26 @@
-from textwrap import dedent
-
 import pytest
+
+from .aoc_2020_day15 import NumberGame
+
+
+GAME_TEST_CASES = [
+    ("0,3,6", 436, 175594),
+    ("1,3,2", 1, 2578),
+    ("2,1,3", 10, 3544142),
+    ("1,2,3", 27, 261214),
+    ("2,3,1", 78, 6895259),
+    ("3,2,1", 438, 18),
+    ("3,1,2", 1836, 362),
+]
+
+
+@pytest.mark.parametrize(("starting_nums", "check_val_2020", "check_val_30m"), GAME_TEST_CASES)
+def test_numbers_game(starting_nums: str, check_val_2020: int, check_val_30m: int) -> None:
+    """Check the game, instantiated with the provided starters, after 2020 and 30 million steps."""
+    game_2020 = NumberGame(starting_nums)
+    game_2020.step_n(2020)
+    assert game_2020.last_seen == check_val_2020
+
+    game_30m = NumberGame(starting_nums)
+    game_30m.step_n(30_000_000)
+    assert game_30m.last_seen == check_val_30m
