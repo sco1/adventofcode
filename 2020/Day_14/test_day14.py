853a239c5eaeda299ac6f40fc7c8c198de575e77
S. Co1 <sco1.git@gmail.com>
2020-12-20 21:12:25 -0500

    Add 2020 Day 14
    Swap in flake8-fixme for flake8-todo, the latter was causing false positives for puzzle input literals with "XXX" and the project hasn't seen activity in a long time.

--- 2020/Day_14/test_day14.py
+++ 2020/Day_14/test_day14.py
@@ -1,3 +1,37 @@
 from textwrap import dedent
 
-import pytest
+from .aoc_2020_day14 import FerryComputer
+
+
+SAMPLE_PROGRAM = dedent(
+    """\
+    mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
+    mem[8] = 11
+    mem[7] = 101
+    mem[8] = 0
+    """
+)
+
+
+def test_part_one() -> None:  # noqa: D103
+    computer = FerryComputer(SAMPLE_PROGRAM)
+    computer.run_program()
+
+    assert computer.initialization_value == 165
+
+
+PART_TWO_SAMPLE = dedent(
+    """\
+    mask = 000000000000000000000000000000X1001X
+    mem[42] = 100
+    mask = 00000000000000000000000000000000X0XX
+    mem[26] = 1
+    """
+)
+
+
+def test_part_two() -> None:  # noqa: D103
+    computer = FerryComputer(PART_TWO_SAMPLE, is_v2=True)
+    computer.run_program()
+
+    assert computer.initialization_value == 208
