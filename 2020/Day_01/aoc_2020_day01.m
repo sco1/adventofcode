d432ff145205789f7958f8ca7f76042f5cc19b82
S. Co1 <sco1.git@gmail.com>
2021-02-01 20:03:32 -0500

    Add 2020 Day 2, MATLAB

--- 2020/Day_01/aoc_2020_day01.m
+++ 2020/Day_01/aoc_2020_day01.m
@@ -7,9 +7,9 @@ fprintf('Part Two: %u\n', find_entry_product(expense_report, target_value, 3));
 
 function [expense_report] = parse_expense_report(filepath)
 % Parse the provided expense report file into an nx1 array of integers
-    arguments
-        filepath char {mustBeFile}
-    end
+arguments
+    filepath char {mustBeFile}
+end
 
 fid = fopen(filepath, 'r');
 expense_report = fscanf(fid, '%lu');  % Use 64-bit to keep values as integers vs. double
@@ -21,11 +21,11 @@ function [result_prod] = find_entry_product(expense_report, target, n_values)
 % Iterate over all combinations of N_VALUES expense values from the provided expense report
 % until a combination is found that sums to the provided TARGET value, then return the product
 % of the value combination.
-    arguments
-        expense_report (1, :)
-        target {mustBePositive}
-        n_values {mustBePositive}
-    end
+arguments
+    expense_report (1, :)
+    target {mustBePositive}
+    n_values {mustBePositive}
+end
 
 check_combinations = nchoosek(expense_report, n_values);
 sums = sum(check_combinations, 2);  % Row-wise summation
