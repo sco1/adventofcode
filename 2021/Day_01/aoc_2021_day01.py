13235d0209cf73537b626c3618c0c65d35a467fc
S. Co1 <sco1.git@gmail.com>
2021-12-02 11:30:15 -0500

    Simplify 2021 Day 1 Logic
    Just change the Python version, leave the other languages' approaches for posterity

--- 2021/Day_01/aoc_2021_day01.py
+++ 2021/Day_01/aoc_2021_day01.py
@@ -3,27 +3,18 @@ from pathlib import Path
 import more_itertools as mi
 
 
-def count_adjacent(soundings: list[int]) -> int:
-    """Count the number of times the depth sounding increases from the previous measurement."""
+def count_ascending(soundings: list[int], width: int = 1) -> int:
+    """
+    Count the number of times the sum of a sliding window increases from the previous window.
+
+    A window width of 1 can be used to consider adjacent values.
+    """
     n_ascending = 0
-    for left, right in mi.sliding_window(soundings, 2):
+    # When comparing the sums of adjacent sliding windows, they share the middle value(s) so the
+    # only delta we need to consider is between the valuies on the boundary of their combined window
+    for left, *_, right in mi.sliding_window(soundings, width + 1):
         if right > left:
             n_ascending += 1
-
-    return n_ascending
-
-
-def count_sums(soundings: list[int], width: int = 3) -> int:
-    """Count the number of times the sum of a sliding window increases from the previous."""
-    last_sum = None
-    n_ascending = 0
-    for window in mi.sliding_window(soundings, width):
-        current_sum = sum(window)
-        if last_sum is not None:  # Skip first iteration
-            if current_sum > last_sum:
-                n_ascending += 1
-
-        last_sum = current_sum
 
     return n_ascending
 
@@ -32,8 +23,8 @@ if __name__ == "__main__":
     puzzle_input_file = Path("./puzzle_input.txt")
     puzzle_input = [int(line) for line in puzzle_input_file.read_text().splitlines()]
 
-    n_ascending_1 = count_adjacent(puzzle_input)
+    n_ascending_1 = count_ascending(puzzle_input, 1)
     print(f"Part One: {n_ascending_1}")
 
-    n_ascending_2 = count_sums(puzzle_input, 3)
+    n_ascending_2 = count_ascending(puzzle_input, 3)
     print(f"Part Two: {n_ascending_2}")
