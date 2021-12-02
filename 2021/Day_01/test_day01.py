c49f1e8a63cdc82559154edaa760bd4f1683ad81
S. Co1 <sco1.git@gmail.com>
2021-12-02 11:30:15 -0500

    Simplify 2021 Day 1 Logic
    Just change the Python version, leave the other languages' approaches for posterity

--- 2021/Day_01/test_day01.py
+++ 2021/Day_01/test_day01.py
@@ -1,6 +1,6 @@
 from textwrap import dedent
 
-from .aoc_2021_day01 import count_adjacent, count_sums
+from .aoc_2021_day01 import count_ascending
 
 SAMPLE_INPUT = dedent(
     """\
@@ -21,12 +21,12 @@ PUZZLE_INPUT = [int(line) for line in SAMPLE_INPUT]
 
 
 def test_part_one() -> None:
-    n_ascending = count_adjacent(PUZZLE_INPUT)
+    n_ascending = count_ascending(PUZZLE_INPUT, 1)
 
     assert n_ascending == 7
 
 
 def test_part_two() -> None:
-    n_ascending = count_sums(PUZZLE_INPUT)
+    n_ascending = count_ascending(PUZZLE_INPUT, 3)
 
     assert n_ascending == 5
