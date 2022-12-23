054fabc2aa939d70a9dcb776a201f6e2034670bf
S. Co1 <sco1.git@gmail.com>
2022-12-23 14:54:03 -0500

    Add 2022 Day 22 Pt 1, Python

--- 2022/Day_22/test_day22.py
+++ 2022/Day_22/test_day22.py
@@ -1,5 +1,7 @@
 from textwrap import dedent
 
+from .aoc_2022_day22 import find_password, parse_puzzle_input
+
 SAMPLE_INPUT = dedent(
     """\
             ...#
@@ -18,3 +20,8 @@ SAMPLE_INPUT = dedent(
     10R5L5R10L4R5L5
     """
 )
+
+
+def test_part_one() -> None:
+    floor, walls, bounds, instructions = parse_puzzle_input(SAMPLE_INPUT)
+    assert find_password(floor, walls, bounds, instructions) == 6032
