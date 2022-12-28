3b388bdfdd7895df96672187dbd7cd109979e514
S. Co1 <sco1.git@gmail.com>
2022-12-28 13:22:55 -0500

    Add 2022 Day 25, Python

--- 2022/Day_25/test_day25.py
+++ 2022/Day_25/test_day25.py
@@ -1,4 +1,8 @@
+from textwrap import dedent
+
 import pytest
+
+from .aoc_2022_day25 import calculate_fuel_requirement, dec2sanfu, snafu2dec
 
 DECIMAL_TO_SNAFU = (
     (1, "1"),
@@ -21,7 +25,7 @@ DECIMAL_TO_SNAFU = (
 
 @pytest.mark.parametrize(("decimal_val", "truth_snafu"), DECIMAL_TO_SNAFU)
 def test_decimal_to_snafu(decimal_val: int, truth_snafu: str) -> None:
-    ...
+    assert dec2sanfu(decimal_val) == truth_snafu
 
 
 SNAFU_TO_DECIMAL = (
@@ -41,6 +45,29 @@ SNAFU_TO_DECIMAL = (
 )
 
 
-@pytest.mark.parametrize(("snafu_val", "truth_snafu"), SNAFU_TO_DECIMAL)
+@pytest.mark.parametrize(("snafu_val", "truth_decimal"), SNAFU_TO_DECIMAL)
 def test_snafu_to_decimal(snafu_val: str, truth_decimal: int) -> None:
-    ...
+    assert snafu2dec(snafu_val) == truth_decimal
+
+
+SAMPLE_INPUT = dedent(
+    """\
+    1=-0-2
+    12111
+    2=0=
+    21
+    2=01
+    111
+    20012
+    112
+    1=-1=
+    1-12
+    12
+    1=
+    122
+    """
+)
+
+
+def test_part_one() -> None:
+    assert calculate_fuel_requirement(SAMPLE_INPUT) == ("2=-1=0", 4890)
